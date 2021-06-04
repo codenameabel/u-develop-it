17c393caac077ae3f58738367ec5938e0363680c
Your Name <you@example.com>
2021-06-03 20:44:32 -0700

    added callback function for sql queries

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const mysql = require('mysql2');
+
 const express = require ('express');
+const { PassThrough } = require('stream');
 
 const PORT = process.env.PORT || 3001;
 const app = express(); 
@@ -6,6 +9,23 @@ const app = express();
 // Express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your mysql username
+        user: 'root',
+        // Your mysql password
+        password: 'th1znation',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err,rows) => { 
+    console.log(rows);
+});
 
 //  Default response for any other request (NOT FOUND)
 app.use((req, res) => { 
