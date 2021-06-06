7bff39b5bc2178c8f20366ab61eeccc7c26d5100
Your Name <you@example.com>
2021-06-05 18:03:28 -0700

    JOINED candidate and party table with first foreign key

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { PassThrough } = require('stream');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+const app = express(); 
 
 // Express middleware 
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +26,12 @@ const db = mysql.createConnection(
 
 // GET all the candidates and add api endpoint for Express.js
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id`;
+    
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -42,7 +47,12 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate and add api endpoint for Express.js
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name
+                 FROM candidates 
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
